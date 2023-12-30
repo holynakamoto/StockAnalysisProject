8f7c53ef965e34c37f46769b49923d5d17ccd3f1
Nick Moore <nick.moore@pm.me>
2023-12-30 11:54:06 -0800

    adding fitting and prediction logic

--- src/model_selection/model_selector.py
+++ src/model_selection/model_selector.py
@@ -1,20 +1,21 @@
-# src/model_selection/model_selector.py
-from sklearn.ensemble import RandomForestRegressor 
+from sklearn.ensemble import RandomForestRegressor
 from keras.models import Sequential
 from keras.layers import SimpleRNN as KerasSimpleRNN, LSTM as KerasLSTM
 from statsmodels.tsa.arima_model import ARIMA
 from statsmodels.tsa.statespace.sarimax import SARIMAX
 
+
 # Base Model
 class BaseModel:
     def __init__(self):
-        pass 
+        pass
 
     def fit(self, X, y):
         pass
 
     def predict(self, X):
         pass
+
 
 # Various models
 class SimpleRNN(BaseModel):
@@ -23,53 +24,55 @@ class SimpleRNN(BaseModel):
         self.model.add(KerasSimpleRNN(units))
 
     def fit(self, X, y):
-        # You should implement the fitting logic here using X and y
-        pass
-        
+        self.model.compile(loss="mean_squared_error", optimizer="adam")
+        self.model.fit(X, y, epochs=10)  # You can adjust the number of epochs as needed
+
     def predict(self, X):
-        # You should implement the prediction logic here using X
-        pass
+        return self.model.predict(X)
+
 
 class LSTM(BaseModel):
-    def __init__(self, units=64):  
+    def __init__(self, units=64):
         self.model = Sequential()
         self.model.add(KerasLSTM(units))
 
     def fit(self, X, y):
-        # You should implement the fitting logic here using X and y
-        pass
-        
-    def predict(self, X):
-        # You should implement the prediction logic here using X
-        pass
+        self.model.compile(loss="mean_squared_error", optimizer="adam")
+        self.model.fit(X, y, epochs=10)  # You can adjust the number of epochs as needed
 
-class ARIMAModel(BaseModel):  
+    def predict(self, X):
+        return self.model.predict(X)
+
+
+class ARIMAModel(BaseModel):
     def __init__(self, order=(1, 0, 0)):
         self.model = ARIMA(order=order)
 
     def fit(self, X, y):
-        # You should implement the fitting logic here using X and y
-        pass
-        
+        self.model = self.model.fit(X, y)
+
     def predict(self, X):
-        # You should implement the prediction logic here using X
-        pass
+        # Assuming X is a pandas DataFrame with appropriate columns, you can use the fitted ARIMA model to predict
+        return self.model.predict(start=X.index[0], end=X.index[-1], dynamic=False)
+
 
 class SARIMAXModel(BaseModel):
     def __init__(self, order=(1, 0, 0), seasonal_order=(1, 0, 0, 12)):
         self.model = SARIMAX(order=order, seasonal_order=seasonal_order)
 
     def fit(self, X, y):
-        # You should implement the fitting logic here using X and y
-        pass
-        
+        self.model = self.model.fit(X, y)
+
     def predict(self, X):
-        # You should implement the prediction logic here using X
-        pass
+        # Assuming X is a pandas DataFrame with appropriate columns, you can use the fitted SARIMAX model to predict
+        return self.model.predict(start=X.index[0], end=X.index[-1], dynamic=False)
+
 
 class RandomForestRegressorModel(BaseModel):
     def __init__(self, n_estimators=100, max_depth=None):
-        self.model = RandomForestRegressor(n_estimators=n_estimators, max_depth=max_depth)
+        self.model = RandomForestRegressor(
+            n_estimators=n_estimators, max_depth=max_depth
+        )
 
     def fit(self, X, y):
         self.model.fit(X, y)
